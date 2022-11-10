114952d753a86ef80c97634d98937cc59343cb03
tupoiu <54478352+tupoiu@users.noreply.github.com>
2022-11-10 11:18:47 +0000

    Started the flat torus version of the Thomson problem

--- src/Algorithms/thomson_problem.py
+++ src/Algorithms/thomson_problem.py
@@ -2,11 +2,15 @@ import math
 import numpy as np
 import random as rd
 
-exponent_s = 2  # The exponent for the force
+exponent_s = 1  # The exponent for the energy (exponent for force is 1+exponent_s)
 
 
 def mag(x):
     return np.sqrt(x.dot(x))
+
+
+def mag2(x):
+    return x.dot(x)
 
 
 def random_points_on_sphere(number: int, radius: float = 1) -> list:
@@ -25,16 +29,33 @@ def random_points_on_sphere(number: int, radius: float = 1) -> list:
 
 
 def normalise(point: np.array):
-    norm_sq = sum([point[i] * point[i] for i in range(3)])
+    norm_sq = sum([point[i] * point[i] for i in range(len(point))])
     return point / math.sqrt(norm_sq)
 
 
-def force(target: np.array, repulsive_point: np.array) -> np.array:  # Force on point0 from point 1
+def force(target: np.array, repulsive_point: np.array) -> np.array:  # Force on target from repulsive force
     dist = mag(target - repulsive_point)
     if dist == 0:
         return target - repulsive_point
     else:
-        return (target - repulsive_point)/math.pow(dist, exponent_s)
+        return (target - repulsive_point)/math.pow(dist, exponent_s+2)  # +1 from differentiating, +1 from normalising
+
+
+def periodic_force(target: np.array, repulsive_point: np.array, boundary: np.array) -> np.array:  # 2D Flat torus force
+    # | x y     | : abs(x - y)            : |x-y|<= boundary/2 : wrap = 0
+    # | x     y | : abs(x + boundary - y) : y - x > boundary/2 : wrap = 1
+    # | y     x | : abs(x - boundary - y) : x - y > boundary/2 : wrap = -1
+    wrap = np.array([
+        int(target[i] - repulsive_point[i] < -boundary[i]/2) - int(target[i] - repulsive_point[i] > boundary[i]/2)
+        for i in range(len(target))
+    ])
+    diff = target - repulsive_point + boundary*wrap
+    dist = mag(diff)
+    if dist == 0:
+        return target - repulsive_point
+    else:
+        return diff/math.pow(dist, exponent_s+2)
+        # s+2: +1 from differentiating, +1 from normalising
 
 
 def thomson_force_step(points: list[np.array], learning_rate=0.001):
@@ -50,12 +71,26 @@ def thomson_force_step(points: list[np.array], learning_rate=0.001):
         points[i] = normalise(points[i])
 
 
+def thomson_flat_force_step(points: list[np.array], boundary: list, learning_rate=0.001):
+    n = len(points)
+    forces = [0 for _ in range(n)]
+    for i in range(n):
+        for j in range(i, n):
+            forces[i] += periodic_force(points[i], points[j], boundary)
+            forces[j] += periodic_force(points[j], points[i], boundary)
+
+    for i in range(n):
+        points[i] += learning_rate*forces[i]
+        for cpnt, j in enumerate(points[i]):  # Push each component back onto the representative domain
+            points[i][j] += (cpnt < 0) * boundary[j] - (cpnt > boundary[j]) * boundary[j]
+
+
 def pairwise_energy(point0: np.array, point1: np.array):
     dist = mag(point0 - point1)
     if dist == 0:
         return 0
     else:
-        return 1 / math.pow(dist, exponent_s-1)
+        return 1 / math.pow(dist, exponent_s)
 
 
 def thomson_energy(points: list[np.array]):
--- test/Algorithms/test_thomson_problem.py
+++ test/Algorithms/test_thomson_problem.py
@@ -24,11 +24,11 @@ class Test(TestCase):
         self.assertTrue(
             th.mag(th.normalise(point) - np.array([2 / math.sqrt(5), 1 / math.sqrt(5), 0])) < 0.001)
 
-    def test_force(self):
+    def test_force(self): # Force is 1/d^2
         point0 = np.array([1, 0, 0])
         point1 = np.array([-1, 0, 0])
         self.assertTrue(
-            th.mag(th.force(point0, point1) - np.array([0.5, 0, 0])) < 0.001
+            th.mag(th.force(point0, point1) - np.array([0.25, 0, 0])) < 0.001
         )
 
     def test_thomson_force_step(self):
@@ -42,3 +42,21 @@ class Test(TestCase):
         self.assertTrue(
             th.mag(points[1] - np.array([-1, 0, 0])) < 0.001
         )
+
+    def test_periodic_force(self):
+        point0 = np.array([0.3], dtype=float)
+        point1 = np.array([0.2], dtype=float)
+        boundary = np.array([1])
+        self.assertTrue(
+            th.mag(th.periodic_force(point0, point1, boundary) - np.array([100])) < 0.001
+        )
+        point0 = np.array([1.9], dtype=float)
+        point1 = np.array([0.1], dtype=float)
+        boundary = np.array([2])
+        print(th.periodic_force(point0, point1, boundary))
+        self.assertTrue(
+            th.mag(th.periodic_force(point0, point1, boundary) - np.array([-1/(0.2*0.2)])) < 0.001
+        )
+
+    def test_thomson_flat_force_step(self):
+        self.fail()
